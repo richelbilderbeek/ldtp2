39026d060b507e006e2cba6ca3829b6e314ff160
Richel Bilderbeek <richelbilderbeek@users.noreply.github.com>
2019-02-01 09:00:18 +0100

    Added suggestion for https://github.com/ldtp/ldtp2/issues/37#issuecomment-456263883

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,3 +1,7 @@
+# Added for https://github.com/ldtp/ldtp2/issues/37#issuecomment-456263883
+import sys
+print(sys.executable)
+
 # from https://github.com/ldtp/ldtp2/issues/35#issuecomment-297901043
 
 from ldtp import *
